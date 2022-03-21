a23138d51f2105850d9ce63764964a0ec51026e8
xhayper <hayper1919@gmail.com>
2022-03-21 09:58:00 +0700

    WOOOOOOO

--- production.bat
+++ production.bat
@@ -1,7 +1,2 @@
-@echo off
-dotnet build --output .output/
-RMDIR /s /q production >nul 2>&1
-XCOPY .output\InscryptionRichPresence.dll production\ /v /y >nul
-XCOPY manifest.json production\ /v /y >nul
-XCOPY icon.png production\ /v /y >nul
-XCOPY README.md production\ /v /y >nul
+@ECHO OFF
+dotnet publish --configuration Release
